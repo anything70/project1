37ac698876e1aaf95e462d45406a239106c877bb
dyoo <98921650+DavJae@users.noreply.github.com>
2025-05-19 14:30:49 -0500

    added a class

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 #testing testing
 #anyone here?
+
+class FinanceTable:
+    def __init__(self):
+        self._value = value
+        self._category = category
+        self._date = date
+
+    #def returnTable(self):
+    #  print(f"")
