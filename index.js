234fd63bfe6d25d110941cfee72307e6d9cecd20
Jonathan Hahn <jonhahn529@gmail.com>
2025-05-21 18:08:11 -0500

    css changes

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database("finances.db",sqlite3.OPEN_READWRITE, (err)=> {
     if (err) return console.error(err.message);
-});
+}); //opens database
 
 let sql = 'CREATE TABLE IF NOT EXISTS finances (id INTEGER PRIMARY KEY, name TEXT, value REAL)'
 db.run(sql)
+//creates database and doesn't return error if already exists
 
 sql = "INSERT INTO finances (name, value) values (?,?)"
-
-
 db.run(sql,[test, 1], (err)=> {
     if (err) return console.error(err.message);
 })
+//inserts list values into database
 
 sql = "SELECT * FROM finances";
 db.all(sql, [], (err, rows) => {
@@ -22,3 +22,9 @@ db.all(sql, [], (err, rows) => {
         console.log(row);
     });
 });
+//queries from db 
+
+
+// sql = "DROP TABLE IF EXISTS finances"
+// db.run(sql)
+// ^^ KEEP AS COMMENT, ONLY USE IF WANT TO DELETE DB!
