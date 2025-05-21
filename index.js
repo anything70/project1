b591a7ba392c264e8ec79fffeb24c9779544ac31
dyoo <98921650+DavJae@users.noreply.github.com>
2025-05-21 18:07:31 -0500

    changes to js catch info from the html form

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-import sqlite3 from "sqlite3";
+/*import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database("finances.db",sqlite3.OPEN_READWRITE, (err)=> {
     if (err) return console.error(err.message);
@@ -9,8 +9,22 @@ let sql = 'CREATE TABLE IF NOT EXISTS finances (id INTEGER PRIMARY KEY, name TEX
 db.run(sql)
 
 sql = "INSERT INTO finances (name, value) values (?,?)"
+*/
+console.log("JS loaded")
+const form = document.getElementById("financeForm");
+if (!form) {
+    console.error("Form not found");
+}
+document.getElementById("financeForm").addEventListener("submit", function(){
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const amount = document.getElementById("amount").value;
+    console.log(name, amount)
+})
 
 
+
+/*
 db.run(sql,[test, 1], (err)=> {
     if (err) return console.error(err.message);
 })
@@ -22,3 +36,4 @@ db.all(sql, [], (err, rows) => {
         console.log(row);
     });
 });
+*/
